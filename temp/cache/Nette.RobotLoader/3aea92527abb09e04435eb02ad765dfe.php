f97d7efe14c97a1c771940c272a1fdf4f9d141b9
KubaCieslarT <kuba.cieslar@seznam.cz>
2019-12-05 21:14:27 +0100

    fokin david

---
+++ temp/cache/Nette.RobotLoader/3aea92527abb09e04435eb02ad765dfe.php
@@ -0,0 +1,62 @@
+<?php
+return array (
+  0 => 
+  array (
+    'App\\Forms\\FormFactory' => 
+    array (
+      'file' => 'C:\\development\\xampp\\htdocs\\app\\forms\\FormFactory.php',
+      'time' => 1575575650,
+    ),
+    'App\\Forms\\SignInFormFactory' => 
+    array (
+      'file' => 'C:\\development\\xampp\\htdocs\\app\\forms\\SignInFormFactory.php',
+      'time' => 1575575650,
+    ),
+    'App\\Forms\\SignUpFormFactory' => 
+    array (
+      'file' => 'C:\\development\\xampp\\htdocs\\app\\forms\\SignUpFormFactory.php',
+      'time' => 1575575650,
+    ),
+    'App\\Model\\UserManager' => 
+    array (
+      'file' => 'C:\\development\\xampp\\htdocs\\app\\model\\UserManager.php',
+      'time' => 1575575650,
+    ),
+    'App\\Model\\DuplicateNameException' => 
+    array (
+      'file' => 'C:\\development\\xampp\\htdocs\\app\\model\\UserManager.php',
+      'time' => 1575575650,
+    ),
+    'App\\Presenters\\BasePresenter' => 
+    array (
+      'file' => 'C:\\development\\xampp\\htdocs\\app\\presenters\\BasePresenter.php',
+      'time' => 1575575650,
+    ),
+    'App\\Presenters\\Error4xxPresenter' => 
+    array (
+      'file' => 'C:\\development\\xampp\\htdocs\\app\\presenters\\Error4xxPresenter.php',
+      'time' => 1575575650,
+    ),
+    'App\\Presenters\\ErrorPresenter' => 
+    array (
+      'file' => 'C:\\development\\xampp\\htdocs\\app\\presenters\\ErrorPresenter.php',
+      'time' => 1575575650,
+    ),
+    'App\\Presenters\\HomepagePresenter' => 
+    array (
+      'file' => 'C:\\development\\xampp\\htdocs\\app\\presenters\\HomepagePresenter.php',
+      'time' => 1575575650,
+    ),
+    'App\\Presenters\\SignPresenter' => 
+    array (
+      'file' => 'C:\\development\\xampp\\htdocs\\app\\presenters\\SignPresenter.php',
+      'time' => 1575575650,
+    ),
+    'App\\RouterFactory' => 
+    array (
+      'file' => 'C:\\development\\xampp\\htdocs\\app\\router\\RouterFactory.php',
+      'time' => 1575575651,
+    ),
+  ),
+  1 => NULL,
+);
